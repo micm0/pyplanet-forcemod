bb09a353e4ee9a764fba8d9f8d986aa231e587ea
El Mezrichi Monir <monir.elmezrichi@hotmail.com>
2020-09-07 16:31:34 +0200

    Added the app code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pyplanet-forcemod
-Pyplanet app that can force a mod url for all maps on a server
+PyPlanet app that can force a mod url for all maps on a ManiaPlanet server.
+
+## Installation
+1. Extract the `forcemod` folder at pyplanet/apps/
+2. Enable the app as usual in: pyplanet/settings/apps.py
+```"apps.mxmod"```
+3. Restart pyplanet
+
+## Configuration
+You need to add the url of the mod ingame in the pyplanet settings (by typing //settings in the chat) next to `Mod url` then skip the map to apply the mod change.
+
+If the url is empty or invalid, the forced mod is unset.
+
+(Works only on the Stadium environment at the moment)
---
+++ forcemod/__init__.py
@@ -0,0 +1,71 @@
+import aiohttp
+import logging
+import asyncio
+from pyplanet.apps.config import AppConfig
+from pyplanet.apps.core.maniaplanet import callbacks as mp_signals
+from pyplanet.contrib.setting import Setting
+from pyplanet import __version__ as pyplanet_version
+
+class ForceModApp(AppConfig):
+	"""
+	Force Mod plugin
+	"""
+	game_dependencies = ['trackmania']
+	app_dependencies = ['core.maniaplanet', 'core.trackmania']
+
+
+	def __init__(self, *args, **kwargs):
+		"""
+		Initializes the plugin.
+		"""
+		super().__init__(*args, **kwargs)
+		self.context.signals.listen(mp_signals.flow.podium_start, self.podium_start)
+		self.mod_url = "http://www.maniapark.com/download/mods/TMT_stadium.zip"
+
+		self.setting_mod_url = Setting(
+			'mod_url', 'Mod Url', Setting.CAT_KEYS, type=str,
+			description='The mod link to be forced (Skip the map to apply the mod change).',
+		)
+		
+	async def on_start(self):
+
+		# Register settings.
+		await self.context.setting.register(
+			self.setting_mod_url,
+		)
+
+		# Force reload the setting.
+		await self.reload_setting()
+
+		
+
+	async def podium_start(self, *args, **kwargs):
+		# Force reload of setting.
+		await self.reload_setting()
+
+			
+	async def reload_setting(self, *args, **kwargs):
+		# Verify force mod setting and get/set mod url.
+			url = await self.setting_mod_url.get_value()
+			if not url:
+				await self.instance.chat('$f00$iForceMod App : Invalid Mod URL, none given! Forced Mod Unset.')
+				await self.instance.gbx.execute("SetForcedMods", False, [])
+			else:
+				with aiohttp.ClientSession(headers={'User-Agent': 'PyPlanet/{}'.format(pyplanet_version)}) as session:
+					try:
+						async with session.get(url) as _:
+							self.mod_url = url
+							await self.instance.gbx.execute("SetForcedMods", True, [
+								{
+									"Env": "Stadium",
+									"Url": self.mod_url
+								}
+							])
+							logging.info('ForceMod App : Next Mod Url : ' + url)
+					except Exception as e:
+						await self.instance.chat('$f00$iForceMod App : Invalid Mod URL ! Forced Mod Unset.')
+						logging.error('Error with checking Mod URL :')
+						logging.error(e)
+						await self.instance.gbx.execute("SetForcedMods", False, [])
+						
+				
